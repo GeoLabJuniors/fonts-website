0f990d006fbef101e49de639578d00df57b847fb
kote-d <didebulidze88@gmail.com>
2017-05-09 01:09:24 +0400

    შრიფტის მაგალითი გავასწორე ცენტრში და ზომებში
    შრიფტის მაგალითი გავასწორე ცენტრში და ზომებში. (მემგონი :P)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
-(function() {
-            [].slice.call(document.querySelectorAll('select.cs-select')).forEach(function(el) {
-                new SelectFx(el);
-            });
-        })();
-        
-        
-        
-        $(".size").on("input change", function(){
-            var size = $(this).val();
-            $(this).parents(".font").children(".row").children(".font-example").children(".mgr0").children(".font-text").css("font-size", size+"em");
-        })
-        
-        $(".weight").on("input change", function(){
-            var weight = $(this).val();
-            console.log(weight);
-            $(this).parents(".font").children(".row").children(".font-example").children(".mgr0").children(".font-text").css("font-weight", weight);
-        })
-        
-        $(".download-btn").mouseover(function(){
-            $(this).parents(".font").children("div.font-hover").css("display", "block");
-            $(this).css("opacity", "0");
-        })
-        
-        $(".download-btn").mouseleave(function(){
-            $(this).parents(".font").children("div.font-hover").css("display", "none");
-            $(this).css("opacity", "1");
-        })
+(function () {
+            [].slice.call(document.querySelectorAll('select.cs-select')).forEach(function (el) {
+        new SelectFx(el);
+    });
+})();
+
+
+
+$(".size").on("input change", function () {
+    var size = $(this).val();
+    $(this).parents(".font").children(".row").children(".font-example").children(".mgr0").children(".font-text").css("font-size", size + "em");
+})
+
+$(".weight").on("input change", function () {
+    var weight = $(this).val();
+    $(this).parents(".font").children(".row").children(".font-example").children(".mgr0").children(".font-text").css("font-weight", weight);
+})
+
+$(".download-btn").mouseover(function () {
+    $(this).parents(".font").children("div.font-hover").css("display", "block");
+    $(this).css("opacity", "0");
+})
+
+$(".download-btn").mouseleave(function () {
+    $(this).parents(".font").children("div.font-hover").css("display", "none");
+    $(this).css("opacity", "1");
+})
+
+$(window).resize(function () {
+    if (innerWidth < 1024 && innerWidth > 992) {
+        $(".size").attr("max", 3.9);
+        $(".size").attr("value", 3.5);
+        $(".font-text").css("font-size", "3.5em");
+
+    }
+})
+
+$(function () {
+    if (innerWidth < 1024 && innerWidth > 992) {
+        $(".size").attr("max", 3.9);
+        $(".size").attr("value", 3.5);
+        $(".font-text").css("font-size", "3.5em");
+
+    }
+})
